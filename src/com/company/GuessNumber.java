76fc5bcee0e5db938ca05f8d49e0fe4d7ac07e93
BingNa Yang <bingnayang@gmail.com>
2020-05-15 22:16:35 -0400

    Add methods and creating testing code

--- src/com/company/GuessNumber.java
+++ src/com/company/GuessNumber.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class GuessNumber {
 
-    private int winningNumber;
+    private int winningNumber  = (int)(Math.random()*(100 - 1)) + 1;
     private int guessingNumber;
     private int countGuess = 10;
 
@@ -10,25 +12,33 @@ public class GuessNumber {
         return winningNumber;
     }
 
-    public void setWinningNumber(int winningNumber) {
-        this.winningNumber = winningNumber;
-    }
-
-    public int getGuessingNumber() {
-        return guessingNumber;
-    }
-
     public void setGuessingNumber(int guessingNumber) {
         this.guessingNumber = guessingNumber;
     }
 
-    public static boolean checkNumber(){
-
-        return true;
+    public int getCountGuess() {
+        return countGuess;
     }
-    public static void startGame(){
-        System.out.println("Welcome to the Number Guessing Game !");
-        System.out.println("You will have 10 chances to guess the winning number");
-        System.out.println("The number you can guess are from 1 - 100 (Include 1 and 100)");
+
+    public boolean checkNumber(){
+        if(this.winningNumber == this.guessingNumber){
+            return true;
+        }
+        else {
+            countGuess--;
+            return false;
+        }
+    }
+    public boolean checkCount(){
+        if(countGuess <= 1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    public void startGame(){
+        System.out.println("----Welcome to the Number Guessing Game !----");
+        System.out.println("----You will have 10 chances to guess the winning number----");
+        System.out.println("----The number you can guess are from 1 - 100 (Include 1 and 100)----");
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,40 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
 	    GuessNumber guessNumber = new GuessNumber();
 	    guessNumber.startGame();
+	    // For Testing
+	    int num = guessNumber.getWinningNumber();
+        System.out.println(num);
+
+        // Declare the variable for number user enter.
+        int number;
+        System.out.println("Enter your first guess number: ");
+        number = scanner.nextInt();
+
+        while (true){
+            guessNumber.setGuessingNumber(number);
+
+            if(guessNumber.checkCount()){
+                if(guessNumber.checkNumber()){
+                    System.out.println("You got it !!");
+                    System.out.println("The winning number is "+guessNumber.getWinningNumber());
+                    break;
+                }
+                System.out.println("Nope, Keep Guessing. Still have "+guessNumber.getCountGuess()+" chances.");
+                System.out.println("Enter the number again: ");
+                number = scanner.nextInt();
+            }else{
+                System.out.println("Too many guessing. Game End");
+                break;
+            }
+        }
     }
 }
